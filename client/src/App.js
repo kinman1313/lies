7b8a64b5ae984bc1efac4b663a72d15fea283bd5
Kevin <kevin@kevinrockwell.dev>
2025-01-06 06:46:51 -0600

    fix: add AuthProvider to App component

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { SocketProvider } from './contexts/SocketContext';
+import { AuthProvider } from './contexts/AuthContext';
 import Login from './components/Login';
 import Register from './components/Register';
 import Chat from './components/Chat';
@@ -10,22 +11,24 @@ import './App.css';
 function App() {
     return (
         <Router>
-            <SocketProvider>
-                <div className="app">
-                    <Routes>
-                        <Route path="/login" element={<Login />} />
-                        <Route path="/register" element={<Register />} />
-                        <Route
-                            path="/"
-                            element={
-                                <PrivateRoute>
-                                    <Chat />
-                                </PrivateRoute>
-                            }
-                        />
-                    </Routes>
-                </div>
-            </SocketProvider>
+            <AuthProvider>
+                <SocketProvider>
+                    <div className="app">
+                        <Routes>
+                            <Route path="/login" element={<Login />} />
+                            <Route path="/register" element={<Register />} />
+                            <Route
+                                path="/"
+                                element={
+                                    <PrivateRoute>
+                                        <Chat />
+                                    </PrivateRoute>
+                                }
+                            />
+                        </Routes>
+                    </div>
+                </SocketProvider>
+            </AuthProvider>
         </Router>
     );
 }
